156692cfdc5d79bc9078aa22d21af4bc49e528bd
Maaz <48104091+zmomz@users.noreply.github.com>
2022-05-16 00:48:00 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,161 @@
+import model
+from ccxt import binance
+import streamlit as st 
+import datetime
+from PIL import Image
+
+image = Image.open('logo.png')
+st.set_page_config(page_title='Wholesale Crypto', page_icon = image, initial_sidebar_state = 'auto')
+
+
+# st.title("Wholesale Crypto")
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
+
+
+img = st.sidebar.image(image)
+
+API = st.sidebar.text_input("Enter API Key")
+SECRET = st.sidebar.text_input("Enter API Secret")
+
+exchange = binance({
+   "apiKey":API, #st.session_state['api_key'],
+   "secret": SECRET,#st.session_state['secret'],
+   "enableRateLimit": True,
+})
+
+
+Select_menu = st.sidebar.selectbox(
+   "Please Select from Menu?",
+   ("Buy Trade", "Sell Trade")
+)
+
+
+###########
+test_mode = st.sidebar.checkbox("Test Mode: ",value=True)
+params = {
+   "test": test_mode  # test if it"s valid, but don"t actually place it
+         }
+
+
+############
+# BUY MODE #
+############
+
+if Select_menu == "Buy Trade":
+   st.markdown("""<style> span[data-baseweb="tag"] {background-color: white !important; color:black !important;}</style>""", unsafe_allow_html=True,)
+
+   # Choosing coins
+   default_coins = []
+   empty_lst=[]
+   all_symbols = list(set(model.get_all_symbols()))
+   special_coins = list(set(model.get_special_coins()))
+   st.subheader("Buy Trade:")
+   selection_mode = st.radio("Select your desired coins:", ('Select All', 'Unselect All', 'Special Coins'))
+
+   if selection_mode == 'Select All':
+      default_coins = all_symbols
+   elif selection_mode == 'Unselect All':
+      default_coins = empty_lst
+   else:
+      default_coins = special_coins
+   
+   order_coins = st.multiselect(" ",all_symbols,default_coins)
+   if selection_mode == 'Special Coins':
+      save_special = st.button("save special coins")
+      if save_special:
+         model.make_special_coins(order_coins, all_symbols)
+   st.write(order_coins)
+   
+
+   # Balance Info
+   usdt_amount = st.number_input("Amount in USDT to trade for each Pair",min_value=0)
+   usdt_balance=model.check_usdt_balance()
+   st.text("Balance information:")
+   st.caption (f"USDT Balance:  {round(usdt_balance)} ")
+   st.caption (f"Number of chosen coins:  {len(order_coins)} ")
+   st.caption (f"Estimated Cost of purchase order: {len(order_coins)*usdt_amount}")
+
+
+   execute = st.button("Execute Orders")
+
+   if(execute):
+      model.create_buy_order(usdt_amount, order_coins, params, exchange)
+
+
+
+
+#############
+# SELL MODE #
+#############
+
+if Select_menu == "Sell Trade":
+   st.markdown("""<style> span[data-baseweb="tag"] {background-color: white !important; color:black !important;}</style>""", unsafe_allow_html=True,)
+
+   st.subheader("Sell Trade:")
+   def update_prices(lst,exchange):
+      for x in lst:
+         model.update_price(x['symbol'],exchange)
+
+   coin_list = model.get_wallet_balance()
+   print(coin_list)
+   col1,col2 =st.columns(2)
+   selection_mode = col1.radio("Choose your selection mode:", ('All', 'Winning Coins', 'Losing Coins'))
+
+   if selection_mode == 'All':
+      selection_list = coin_list
+   elif selection_mode == 'Winning Coins':
+      selection_list = [x for x in coin_list if (x['last_price']*x['volume'] - x['costed'])>0]
+   else:
+      selection_list = [x for x in coin_list if (x['last_price']*x['volume'] - x['costed'])<=0]
+
+   with col2:
+      time_def = datetime.timedelta(hours=3)
+      nowtime = datetime.datetime.now() + time_def
+      formated_time = nowtime.strftime("%H:%M:%S") 
+      st.text("last update: "+ formated_time)
+      st.button("Update prices", on_click= update_prices(coin_list, exchange))
+   
+   def modify_list(x):
+      if x['last_price']* x['volume'] - x['costed'] > 0:
+         label = "🟢  "+ x['symbol'] + "  (+ "  + str(round(x['last_price']*x['volume'] - x['costed'],3))+" )"
+      else:
+         label = "🔻  "+ x['symbol'] + "  ( "  + str(round(x['last_price']*x['volume'] - x['costed'],3))+" )"
+      # label=st.checkbox(label=x['symbol'])
+      return label
+
+   order_list = st.multiselect(label=" ",options=selection_list,default=selection_list, format_func=modify_list)
+
+
+   amount = st.number_input("amount to sell (in USDT): ", min_value=0)
+
+   submit_button = st.button(label="Execute Orders")
+   if submit_button:
+      model.create_sell_order(amount_usdt= amount, options_selected= order_list, params= params,exchange=exchange)
+
+#############
+# Auto SELL #
+# #############
+
+#    if Select_menu == "Auto Sell":
+#       st.subheader("Auto Sell:")
+
+#       col1,col2 =st.columns(2)
+#       mode= col1.radio("choose your auto sell mode:",("wallet total","single coins"))
+#       percentage= col2.slider("Select your Minimum Percentage: ", min_value=0, max_value=20, value=2, step=1)
+#       start= col1.button("start the bot")
+#       stop= col2.button("stop the bot")
+
+
+#       total_cost = 0
+#       total_profit= 0
+#       coin_list = model.get_wallet_balance()
+#       for coin in coin_list:
+#          cost = coin['costed']
+#          profit = coin['last_price']*coin['volume'] - coin['costed']
+#          total_cost += cost
+#          total_profit += profit
+      
---
+++ model.py
@@ -0,0 +1,202 @@
+import streamlit as st
+import sqlite3
+import time
+
+tickers= ['USDT','BTC', 'LTC', 'ETH', 'NEO', 'BNB', 'QTUM', 'EOS', 'BNT', 'DNT', 'ZRX', 'OMG', 'WTC', 'LRC', 'TRX', 'FUN', 'KNC', 'XVG', 'IOTA', 'LINK', 'CVC', 'REP', 'MTL', 'NULS', 'STX', 'ADX', 'ETC', 'ZEC', 'BAT', 'DASH', 'POWR', 'BTG', 'REQ', 'XMR', 'ENJ', 'XRP', 'STORJ', 'KMD', 'DATA', 'MANA', 'BTS', 'LSK', 'ADA', 'XLM', 'WAVES', 'GTO', 'ICX', 'ELF', 'AION', 'RLC', 'IOST', 'STEEM', 'BLZ', 'SYS', 'ONT', 'ZIL', 'XEM', 'WAN', 'ZEN', 'THETA', 'IOTX', 'SC', 'KEY', 'MFT', 'DENT', 'ARDR', 'HOT', 'VET', 'DOCK', 'POLY', 'VTHO', 'ONG', 'RVN', 'DCR', 'MITH', 'REN', 'FET', 'TFUEL', 'CELR', 'MATIC', 'ATOM', 'ONE', 'FTM', 'CHZ', 'COS', 'ALGO', 'DOGE', 'DUSK', 'ANKR', 'WIN', 'COCOS', 'PERL', 'TOMO', 'BAND', 'BEAM', 'HBAR', 'XTZ', 'DGB', 'NKN', 'GBP', 'EUR', 'KAVA', 'ARPA', 'CTXC', 'BCH', 'TROY', 'VITE', 'FTT', 'AUD', 'OGN', 'DREP', 'TCT', 'WRX', 'LTO', 'MBL', 'COTI', 'HIVE', 'STPT', 'SOL', 'CTSI', 'CHR', 'HNT', 'JST', 'FIO', 'STMX', 'MDT', 'PNT', 'COMP', 'IRIS', 'MKR', 'SXP', 'SNX', 'DOT', 'RUNE', 'AVA', 'BAL', 'YFI', 'SRM', 'ANT', 'CRV', 'SAND', 'OCEAN', 'NMR', 'LUNA', 'IDEX', 'RSR', 'PAXG', 'WNXM', 'TRB', 'EGLD', 'KSM', 'SUSHI', 'YFII', 'DIA', 'BEL', 'UMA', 'NBS', 'WING', 'UNI', 'OXT', 'SUN', 'AVAX', 'BURGER', 'BAKE', 'FLM', 'SCRT', 'XVS', 'CAKE', 'ALPHA', 'ORN', 'UTK', 'NEAR', 'VIDT', 'AAVE', 'FIL', 'INJ', 'CTK', 'AUDIO', 'AXS', 'AKRO', 'HARD', 'KP3R', 'SLP', 'STRAX', 'UNFI', 'CVP', 'FOR', 'FRONT', 'ROSE', 'MDX', 'SKL', 'GHST', 'DF', 'JUV', 'PSG', 'GRT', 'CELO', 'TWT', 'REEF', 'OG', 'ATM', 'ASR', '1INCH', 'RIF', 'BTCST', 'TRU', 'DEXE', 'CKB', 'FIRO', 'LIT', 'SFP', 'FXS', 'DODO', 'AUCTION', 'ACM', 'PHA', 'TVK', 'BADGER', 'FIS', 'QI', 'OM', 'POND', 'ALICE', 'DEGO', 'BIFI', 'LINA', 'PERP', 'RAMP', 'SUPER', 'CFX', 'TKO', 'AUTO', 'EPS', 'PUNDIX', 'TLM', 'MIR', 'BAR', 'FORTH', 'AR', 'ICP', 'SHIB', 'POLS', 'MASK', 'LPT', 'ATA', 'GTC', 'KLAY', 'TORN', 'ERN', 'BOND', 'MLN', 'C98', 'FLOW', 'QUICK', 'RAY', 'MINA', 'QNT', 'CLV', 'XEC', 'ALPACA', 'FARM', 'VGX', 'MBOX', 'WAXP', 'TRIBE', 'GNO', 'DYDX', 'GALA', 'ILV', 'YGG', 'FIDA', 'AGLD', 'BETA', 'RAD', 'RARE', 'LAZIO', 'MOVR', 'CHESS', 'DAR', 'ACA', 'ASTR', 'BNX', 'CITY', 'ENS', 'PORTO', 'JASMY', 'AMP', 'PLA', 'PYR', 'SANTOS', 'RNDR', 'ALCX', 'MC', 'VOXEL', 'BICO', 'FLUX', 'UST', 'HIGH', 'OOKI', 'CVX', 'PEOPLE', 'SPELL', 'JOE', 'GLMR', 'ACH', 'IMX', 'LOKA', 'BTTC', 'ANC', 'API3', 'XNO', 'WOO', 'ALPINE', 'T', 'NBT', 'KDA', 'APE', 'GMT', 'BSW', 'MULTI']
+
+############
+# BUY MODE #
+############
+
+def get_balance(tickers,exchange):
+    try:
+        balance_dirty = exchange.fetch_balance()['info']['balances']
+    except Exception as e:
+        return e
+    balance_clean=[]
+    for b in balance_dirty:
+        if b['asset'] in tickers:
+            balance_clean.append(b)
+    return balance_clean
+
+
+def get_last_price(symbol,exchange):
+    if symbol != "USDT":
+        try:
+            last_price = exchange.fetch_ticker(symbol+'/USDT')['last']
+            return last_price
+        except Exception as e:
+            print(e)
+    else:
+        return 1
+
+def create_table():
+    with sqlite3.connect("data.db") as con:
+        cur = con.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS symbols(symbol TEXT, costed REAL, volume_in_wallet REAL, last_price REAL, special REAL)")
+        con.commit()
+
+def update_table(exchange):
+    balance= get_balance(tickers, exchange)
+    with sqlite3.connect("data.db") as con:
+        cur = con.cursor()
+        for pair in balance:
+            last_price = get_last_price(pair['asset'],exchange)
+            cur.execute("INSERT INTO symbols(symbol, costed, volume_in_wallet, last_price, special) VALUES (?,?,?,?,?)", (pair['asset'],0,0,last_price,0))
+            con.commit()
+
+def get_all_symbols():
+    with sqlite3.connect("data.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT symbol FROM symbols")
+        data = [x[0] for x in cur.fetchall() if x[0] != "USDT"]
+        return data
+
+def get_special_coins():
+    with sqlite3.connect("data.db") as con:
+        cur = con.cursor()
+        cur.execute(f"SELECT symbol FROM symbols WHERE special = 1")
+        data = [x[0] for x in cur.fetchall() if x[0] != "USDT"]
+        return data
+
+# def get_special_coins():
+#     with sqlite3.connect("data.db") as con:
+#         cur = con.cursor()
+#         cur.execute("SELECT symbol FROM symbols WHERE special = 1")
+#         data = [x[0] for x in cur.fetchall() if x[0] != "USDT"]
+#         return data
+
+def make_special_coins(selected,all_Symbols):
+    print(selected)
+    with sqlite3.connect("data.db") as con:
+        cur = con.cursor()
+        for symbol in all_Symbols:
+            if symbol in selected:
+                print(symbol)
+                cur.execute(f"UPDATE symbols SET special = 1 WHERE symbol = '{symbol}'")
+            else:
+                cur.execute(f"UPDATE symbols SET special = 0 WHERE symbol = '{symbol}'")
+            con.commit()
+
+# def customise_all_symbols(coin_dict,position):
+#     with sqlite3.connect("data.db") as con:
+#         cur = con.cursor()
+#         for key,value in coin_dict.items():
+#             if value:
+#                 cur.execute(f"UPDATE symbols SET selected_{position} = 1 WHERE symbol = '{key}'")
+#             else:
+#                 cur.execute(f"UPDATE symbols SET selected_{position} = 0 WHERE symbol = '{key}'")
+#         con.commit()
+
+
+def check_usdt_balance():
+    with sqlite3.connect("data.db") as con:
+        cur = con.cursor()
+        cur.execute(f"SELECT volume_in_wallet FROM symbols WHERE symbol = 'USDT'")
+        data = cur.fetchall()[0][0]
+        return data
+
+def update_after_buy(symbol,costed,volume_in_wallet):
+    print("costed",costed,"symbol",symbol,"vol",volume_in_wallet)
+    with sqlite3.connect("data.db") as con:
+        cur = con.cursor()
+        cur.execute(f"UPDATE symbols SET costed = costed + {costed}, volume_in_wallet = volume_in_wallet + {volume_in_wallet} WHERE symbol='{symbol}'")
+        con.commit()
+
+def create_buy_order(usdt_amount,order_coins,params,exchange):
+    selected = order_coins
+    if len(selected)  <= 0 :
+        st.warning("Please Add coin to buy!")
+    else :
+        for coin in selected :
+            coinusdt =coin + "/USDT"
+            try:
+                symbol_ticker = exchange.fetch_ticker(coinusdt)
+                symbol_price=float(symbol_ticker["last"])
+                if symbol_price == 0 :
+                    st.warning (f"Please Remove {coin}")
+                amount = usdt_amount / symbol_price
+            except Exception as e:
+                st.warning(f"{e}")
+                pass
+            
+            try:
+                order=exchange.create_market_order(symbol=coinusdt, side="buy", amount=amount, params=params)
+                print(order)
+                st.success(f"We bought {amount} from coin {coin}")
+                if not params["test"]:
+                    #  order['filled']: of base currency
+                    #  order['price']: USDT
+                    #  order['cost']: Price * filled
+                    update_after_buy(symbol=coin,costed=order['cost'],volume_in_wallet=order['filled'])
+                # st.write(order)
+                time.sleep(0.5)
+            except Exception as e:
+                st.write(f"an exception occured - {e}")
+
+
+#############
+# SELL MODE #
+#############
+
+def get_wallet_balance():
+    with sqlite3.connect("data.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM symbols WHERE costed > 0 AND symbol != 'USDT'")
+        data = cur.fetchall()
+        result=[]
+        for x in data:
+            
+            result.append({
+                "symbol":x[0],
+                "costed":x[1],
+                "volume":x[2],
+                "last_price":x[3]
+            })
+        return result
+
+
+def update_price(symbol,exchange):
+    last_price = get_last_price(symbol,exchange)
+    if last_price:
+        with sqlite3.connect("data.db") as con:
+            cur = con.cursor()
+            cur.execute(f"UPDATE symbols SET last_price = {last_price} WHERE symbol = '{symbol}'")
+            con.commit()
+
+def update_after_sell(symbol,gained,volume_reduction):
+    with sqlite3.connect("data.db") as con:
+        cur = con.cursor()
+        cur.execute(f"UPDATE symbols SET costed = costed - {gained}, volume_in_wallet = volume_in_wallet - {volume_reduction} WHERE symbol='{symbol}'")
+        con.commit()
+
+def create_sell_order(amount_usdt,options_selected,params,exchange):
+    print(options_selected)
+    if len(options_selected) <= 0:
+        st.warning("Please Add coin to buy!")
+    else :
+        for selected in options_selected :
+            amount = amount_usdt / selected['last_price']
+            symbol = selected['symbol'] +'/USDT'            
+            try:
+                order=exchange.create_market_order(symbol=symbol, side="sell", amount=amount, params=params)
+                print(order)
+                st.success(f"We sold {amount} from coin {symbol}")
+                if not params["test"]:
+                    #  order['filled']: of base currency
+                    #  order['price']: USDT
+                    #  order['cost']: Price * filled
+                    update_after_sell(symbol=selected['symbol'],gained=order['cost'],volume_reduction=order['filled'])
+                # st.write(order)
+                time.sleep(0.5)
+            except Exception as e:
+                st.write(f"an exception occured - {e}")
+
+
+
+# {'info': {}, 'id': None, 'clientOrderId': None, 'timestamp': None, 'datetime': None, 'lastTradeTimestamp': None, 'symbol': 'ETH/USDT',
+#  'type': None, 'timeInForce': None, 'postOnly': False, 'side': None, 'price': None, 'stopPrice': None, 'amount': None, 'cost': None,
+#  'average': None, 'filled': 0.0, 'remaining': None, 'status': None, 'fee': None, 'trades': [], 'fees': []}
+
+
+
